5576ac63351be78bc538483ded33fe7428fd32dc
IzharTheDev <IzharProCoder-web>
2025-03-19 13:16:10 +0500

    make  contact hero section

--- my-project/src/Pages/Contact.jsx
+++ my-project/src/Pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ContactHeroComp from '../Components/ContactUs/ContactHeroComp'
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+    <ContactHeroComp />
     </div>
   )
 }
